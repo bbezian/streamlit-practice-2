415eca480d54efe9a083a1856f6ada71ecc50d0d
bbezian <bbezian@yahoo.com>
2023-12-29 15:21:50 -0500

    add

---
+++ predict_page.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pickle
+import joblib
+import numpy as np 
+
+upload_file = st.file_uploader('upload')
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+
+data = load_model()
+
+
+regressor = data['model']
+le_country = data['le_country']
+le_education = data['le_education']
+
+
+def show_predict_page():
+    st.title('Software Developer Salary prediction')
+
+    st.write('''We need some information to predict the salary''')
+
+
+    countries = (
+    'United States of America',
+    'United Kingdom of Great Britain and Northern Ireland',
+    'Other',
+    'Australia',
+    'Netherlands',
+    'Germany',
+    'Sweden',
+    'France',
+    'Spain',
+    'Brazil',
+    'Italy',
+    'Canada',
+    'Switzerland',
+    'India',
+    'Norway',
+    'Denmark',
+    'Israel',
+    'Poland'
+    )
+
+    education = (
+        'Bachelor’s degree', 
+        'Less than a Bachelors', 
+        'Master’s degree',
+        'Post grad'
+    )
+
+
+    country = st.selectbox('country', countries)
+    education = st.selectbox('education Level', education)
+
+    experience = st.slider('Years of Experience', 0, 50, 2)
+
+    ok = st.button('Calculate Salary')
+    if ok:
+        X = np.array([[country, education, experience]])
+        X[:, 0] = le_country.transform(X[:,0])
+        X[:, 1] = le_education.transform(X[:,1])
+        X = X.astype(float)
+
+
+        salary = regressor.predict(X)
+        st.subheader(f'The estimated salary is ${salary[0]:.2f}')
+
+
+
